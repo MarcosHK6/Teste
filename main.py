5b1420066fbb6096a6ae2efbc1381e694f62e75f
MarcosHK6 <koglerhenrique@gmail.com>
2022-02-15 09:55:44 -0300

    API

--- main.py
+++ main.py
@@ -1,34 +1,54 @@
 from fastapi import FastAPI
-from sistema import Cadastro, Sistema
+from sistema import Pessoa, Sistema
+from pydantic import BaseModel
 
 sist = Sistema()
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return
+    return {"Hello": "World"}
 
-@app.post("/pessoa/add")
+@app.get("/pessoa/getall")
+def get_all():
+    return sist.getall()
 
-@app.get("/consulta/{cpf}")
+@app.get("/pessoa/consulta/{cpf}")
 def consulta(cpf: str):
+    if not sist.validacao(cpf):
+        return {"Mensagem": "CPF deve ser informado exclusivamente por números"}
     pessoa = sist.get(cpf)
     if pessoa == None:
-        return "Cadastro não encontrado"
+        return {"Mensagem": "Cadastro não encontrado"}
     else:
         return pessoa
 
 @app.post("/pessoa/add")
-def add_pessoa():
-    return
+def add_pessoa(pessoa: Pessoa):
+    if not sist.validacao(pessoa.cpf, pessoa.idade, pessoa.telefone):
+        return {"Mensagem": "CPF, idade e telefone devem ser informados exclusivamente por números"}
+    if sist.add_pessoa(pessoa):
+        return {"Mensagem": "Cadastro efetuado com sucesso!"}
+    else:
+        return {"Mensagem": "Este CPF já está cadastrado no sistema"}
 
-@app.delete("/pessoa/{cpf}/delete")
+@app.delete("/pessoa/delete/{cpf}")
 def delete_pessoa(cpf: str):
     excluido = sist.excluir(cpf)
     if excluido:
-        return "Cadastro excluído"
+        return {"Mensagem": "Cadastro excluído"}
     else:
-        return "Cadastro não encontrado"
+        return {"Mensagem": "Cadastro não encontrado"}
+
+@app.put("/pessoa/update/{cpf}")
+def update_pessoa(cpf: str, pessoa: Pessoa):
+    if not sist.validacao(pessoa.cpf, pessoa.idade, pessoa.telefone):
+        return {"Mensagem": "CPF, idade e telefone devem ser informados exclusivamente por números"}
+    status = sist.update(cpf, pessoa)
+    if status == 1:
+        return {"Mensagem": "Alteração de cadastro ocorreu com sucesso!"}
+    else:
+        return {"Mensagem": "Cadastro não encontrado"}
 
 @app.get("/relatorio/1")
 def relatorio1():
--- sistema.py
+++ sistema.py
@@ -1,16 +1,22 @@
 import psycopg
 from pydantic import BaseModel
 
-class Cadastro():
-    def __init__(self, cpf, nome, idade, telefone):
-        self.cpf = cpf
-        self.nome = nome
-        self.idade = idade
-        self.telefone = telefone
+#class Cadastro():
+#    def __init__(self, cpf, nome, idade, telefone):
+#        self.cpf = cpf
+#        self.nome = nome
+#        self.idade = idade
+#        self.telefone = telefone
+
+class Pessoa(BaseModel):
+    cpf: str
+    nome: str
+    idade: int
+    telefone: str
 
 class Sistema:
     def __init__(self):
-        self.cadastros = []
+        self.pessoas = {}
         self.info = 'postgresql://postgres:postgres@localhost:5432/Pessoa'
         self.connection = psycopg.connect(conninfo=self.info)
         #self.connection = psycopg.connect(user="postgres",
@@ -20,7 +26,7 @@ class Sistema:
         #                              host="localhost",
 		#							  database="SRS")
 
-    def validacao(self, cpf, idade, telefone):
+    def validacao(self, cpf, idade = 0, telefone = 0):
         try:
             cpf = int(cpf)
             idade = int(idade)
@@ -33,7 +39,8 @@ class Sistema:
             return True
 
     def consulta(self, cpf):
-        for c in self.cadastros:
+        return self.pessoas.get(cpf)
+        for c in self.pessoas:
             if c.cpf == cpf:
                 return c
             else:
@@ -51,35 +58,45 @@ class Sistema:
                 nome = encontrado[1]
                 idade = encontrado[2]
                 telefone = encontrado[3]
-                pessoa = Cadastro(cpf, nome, idade, telefone)
-                self.cadastros.append(pessoa)
+                pessoa = Pessoa(cpf=cpf, nome=nome, idade=idade, telefone=telefone)
+                self.pessoas[cpf] = pessoa
+                cursor.close()
                 return pessoa
             else:
+                cursor.close()
                 return None
         else:
             return pessoa
 
-    def put(self, cpf, nome, idade, telefone):
-        if self.validacao(cpf, idade, telefone):
-            if self.get(cpf) == None:
-                self.create_pessoa(cpf, nome, idade, telefone)
-                return 0
-            else:
-                return 1
-        else:
-            return 2
+    def getall(self):
+        cursor = self.connection.cursor()
+        consulta = "SELECT * FROM PESSOA"
+        cursor.execute(consulta)
+        all = cursor.fetchall()
+        self.connection.commit()
+        cursor.close()
+        return all
 
-    def create_pessoa(self, cpf, nome, idade, telefone):
-        self.cadastros.append(Cadastro(cpf, nome, idade, telefone))
+    def add_pessoa(self, pessoa: Pessoa):
+        if self.get(pessoa.cpf) == None:
+            self.create_pessoa(pessoa)
+            return True
+        else:
+            return False
+
+    def create_pessoa(self, pessoa: Pessoa):
+        self.pessoas[pessoa.cpf] = pessoa
+        #self.pessoas.append(pessoa)
         cursor = self.connection.cursor()
         consulta = "INSERT INTO PESSOA VALUES (%s, %s, %s, %s)"
-        cursor.execute(consulta, (cpf, nome, idade, telefone))
+        cursor.execute(consulta, (pessoa.cpf, pessoa.nome, pessoa.idade, pessoa.telefone))
         self.connection.commit()
         cursor.close()
 
     def excluir(self, cpf):
+        self.pessoas.pop(cpf, None)
         cursor = self.connection.cursor()
-        consulta = f"DELETE FROM PESSOA WHERE CPF = %s"
+        consulta = "DELETE FROM PESSOA WHERE CPF = %s"
         cursor.execute(consulta, (cpf,))
         excluido = cursor.rowcount
         self.connection.commit()
@@ -89,16 +106,29 @@ class Sistema:
         else:
             return False
 
+    def update(self, cpf, pessoa: Pessoa):
+        self.pessoas.pop(cpf, None)
+        self.pessoas[pessoa.cpf] = pessoa
+        cursor = self.connection.cursor()
+        consulta = """UPDATE PESSOA
+                    SET CPF = %s, NOME = %s, IDADE = %s, TELEFONE = %s
+                    WHERE CPF = %s"""
+        cursor.execute(consulta, (pessoa.cpf, pessoa.nome, pessoa.idade, pessoa.telefone, cpf))
+        self.connection.commit()
+        update = cursor.rowcount
+        cursor.close()
+        return update
+
     def relatorio1(self):
         cursor = self.connection.cursor()
-        consulta = f"SELECT IDADE, COUNT(CPF) FROM PESSOA GROUP BY IDADE ORDER BY IDADE"
+        consulta = "SELECT IDADE, COUNT(CPF) FROM PESSOA GROUP BY IDADE ORDER BY IDADE"
         relatorio = cursor.execute(consulta).fetchall()
         cursor.close()
         return relatorio
 
     def relatorio2(self):
         cursor = self.connection.cursor()
-        consulta = f"""SELECT CPF, NOME, IDADE, TELEFONE FROM PESSOA WHERE IDADE = (SELECT MIN(IDADE) FROM PESSOA)
+        consulta = """SELECT CPF, NOME, IDADE, TELEFONE FROM PESSOA WHERE IDADE = (SELECT MIN(IDADE) FROM PESSOA)
                     UNION ALL
                     SELECT CPF, NOME, IDADE, TELEFONE FROM PESSOA WHERE IDADE = (SELECT MAX(IDADE) FROM PESSOA)"""
         relatorio = cursor.execute(consulta).fetchall()
